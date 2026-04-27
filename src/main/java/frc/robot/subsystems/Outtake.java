176de3164b7857160e288f55ae893af2c0bb3338
CharlesCurylo <159273390+CharlesCurylo@users.noreply.github.com>
2026-04-27 17:08:22 -0400

    Change from InstantCommand to command factory method

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -3,14 +3,15 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 
-public class Outtake {
+public class Outtake implements Subsystem {
     private final TalonFX leftFlywheelMotor = new TalonFX(21);
     private final TalonFX rightFlywheelMotor = new TalonFX(20);
     private final TalonFX kickerMotor = new TalonFX(14);
     private final TalonFX conveyorMotor = new TalonFX(15);
 
     public Command shoot() {
-        return new InstantCommand();
+        return runOnce(() -> System.out.println("hi "));
     }
 }
