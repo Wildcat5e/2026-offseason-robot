fd3a3529425b2dd8a835fc31445cb5e6009f91ec
Tommy <aagr2013@gmail.com>
2026-05-06 21:51:35 -0400

    added import statements for subsystem base. removed import statement for subsystem interface since we're extending subsystem base

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Outtake extends SubsystemBase {
     private final TalonFX leftFlywheelMotor = new TalonFX(21);
@@ -15,7 +15,7 @@ public class Outtake extends SubsystemBase {
         // invert motors so they don't spin in circles
         // i forgot the import statement, so leaving this here as reminder
     }
-    
+
     public Command shoot() {
         return runOnce(() -> System.out.println("hi "));
     }
