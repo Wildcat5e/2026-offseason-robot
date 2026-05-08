3b9f2f9d744fa9b3b498078ece3c6615fbd9dd79
CharlesCurylo <159273390+CharlesCurylo@users.noreply.github.com>
2026-05-08 16:51:20 -0400

    Add values to outtake

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -10,15 +10,15 @@ public class Outtake implements Subsystem {
     private final TalonFX rightFlywheelMotor = new TalonFX(20);
     private final TalonFX kickerMotor = new TalonFX(14);
     private final TalonFX conveyorMotor = new TalonFX(15);
-    double flywheelVoltage = 3;
-    double kickerVoltage = 3;
-    double conveyorVoltage = 3;
+    double flywheelVoltage = 2;
+    double kickerVoltage = 4;
+    double conveyorVoltage = 8;
     // replace with real values from on 2026 rebuilt robot if you deploy
 
     public Command shoot() {
         return startEnd(() -> {
             leftFlywheelMotor.setVoltage(flywheelVoltage);
-            rightFlywheelMotor.setVoltage(flywheelVoltage);
+            rightFlywheelMotor.setVoltage(-flywheelVoltage);
             kickerMotor.setVoltage(kickerVoltage);
             conveyorMotor.setVoltage(conveyorVoltage);
         }, () -> {
